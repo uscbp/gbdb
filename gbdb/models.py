d7d116f6639c275ddd7d652ded6d1b483c0435bf
jbonaiuto <jbonaiuto@gmail.com>
2014-08-26 11:27:54 +0100

    refactored video conversion

--- gbdb/models.py
+++ gbdb/models.py
@@ -11,75 +11,32 @@ from uscbp import settings
 from django.utils.encoding import force_unicode
 import os.path
 
-class RenameFilesModel(models.Model):
-    """
-    Abstract model implementing a two-phase save in order to rename
-    `FileField` and `ImageField` filenames after saving.  This allows the
-    final filenames to contain information like the primary key of the model.
+def convert_to_mp4(mp4_filename, orig_filename, start_time=None, duration=None):
+    cmds = ['avconv', '-i', orig_filename]
+    if start_time is not None:
+        cmds.extend(['-ss', start_time])
+    if duration is not None:
+        cmds.extend(['-t', duration])
+    cmds.extend(['-vcodec', 'libx264', mp4_filename])
+    subprocess.call(cmds)
 
-    Example:
+def convert_to_ogg(ogg_filename, orig_filename, start_time=None, end_time=None):
+    cmds=['ffmpeg2theora', orig_filename]
+    if start_time is not None:
+        cmds.extend(['-s',start_time])
+    if end_time is not None:
+        cmds.extend(['-e',end_time])
+    cmds.extend(['-o', ogg_filename])
+    subprocess.call(cmds)
 
-        class Photo(RenameFilesModel):
-            file = models.ImageField(upload_to='uploads/temp')
-
-            RENAME_FILES = {
-                'file': {'dest': 'uploads/photos', 'keep_ext': True}
-            }
-
-        >>> photo = Photo(file='uploads/temp/photo.jpg')
-        >>> photo.pk
-
-        >>> photo.save()
-        >>> photo.pk
-        1
-        >>> photo.file
-        <ImageFieldFile: uploads/photos/1.jpg>
-
-    If the 'dest' option is a callable, it will be called with the model
-    instance (guaranteed to be saved) and the currently saved filename, and
-    must return the new filename.  Otherwise, the filename is determined by
-    'dest' and the model's primary key.
-
-    If a file already exists at the resulting path, it is deleted.  This is
-    desirable if the filename should always be the primary key, for instance.
-    To avoid this behavior, write a 'dest' handler that results in a unique
-    filename.
-
-    If 'keep_ext' is True (the default), the extension of the previously saved
-    filename will be appended to the primary key to construct the filename.
-    The value of 'keep_ext' is not considered if 'dest' is a callable.
-
-    """
-    RENAME_FILES = {}
-
-    class Meta:
-        abstract = True
-
-    def save(self, force_insert=False, force_update=False):
-        rename_files = getattr(self, 'RENAME_FILES', None)
-        if rename_files:
-            super(RenameFilesModel, self).save(force_insert, force_update)
-            force_insert, force_update = False, True
-
-            for field_name, options in rename_files.iteritems():
-                field = getattr(self, field_name)
-                file_name = force_unicode(field)
-                name, ext = os.path.splitext(file_name)
-                keep_ext = options.get('keep_ext', True)
-                final_dest = options['dest']
-                if callable(final_dest):
-                    final_name = final_dest(self, file_name)
-                else:
-                    final_name = os.path.join(final_dest, '%s' % (self.pk,))
-                    if keep_ext:
-                        final_name += ext
-                if file_name != final_name:
-                    field.storage.delete(final_name)
-                    field.storage.save(final_name, field)
-                    field.storage.delete(file_name)
-                    setattr(self, field_name, final_name)
-
-        super(RenameFilesModel, self).save(force_insert, force_update)
+def convert_to_swf(swf_filename, orig_filename, start_time=None, duration=None):
+    cmds=['avconv', '-i', orig_filename]
+    if start_time is not None:
+        cmds.extend(['-ss', start_time])
+    if duration is not None:
+        cmds.extend(['-t', duration])
+    cmds.extend(['-ar', '44100', swf_filename])
+    subprocess.call(cmds)
 
 class Species(models.Model):
     genus_name = models.CharField(max_length=100)
@@ -206,16 +163,13 @@ class ObservationSession(models.Model):
             root,ext=os.path.splitext(orig_filename)
             mp4_filename='%s.mp4' % root
             if not os.path.exists(mp4_filename):
-                cmds=['avconv', '-i', orig_filename, '-vcodec', 'libx264', mp4_filename]
-                subprocess.call(cmds)
+                convert_to_mp4(mp4_filename, orig_filename)
             ogg_filename='%s.ogg' % root
             if not os.path.exists(ogg_filename):
-                cmds=['ffmpeg2theora', orig_filename, '-o', ogg_filename]
-                subprocess.call(cmds)
+                convert_to_ogg(ogg_filename, orig_filename)
             swf_filename='%s.swf' % root
             if not os.path.exists(swf_filename):
-                cmds=['ffmpeg', '-i', orig_filename, swf_filename]
-                subprocess.call(cmds)
+                convert_to_swf(swf_filename, orig_filename)
         
         
 class BehavioralEvent(MPTTModel):
@@ -245,21 +199,19 @@ class BehavioralEvent(MPTTModel):
                                              self.start_time.microsecond)
         duration_string = '%d:%d:%d.%d' % (self.duration.hour, self.duration.minute, self.duration.second,
                                            self.duration.microsecond)
+        end_time=self.start_time+self.duration
+        end_time_string='%d:%d:%d.%d' % (end_time.hour, end_time.minute, end_time.second, end_time.microsecond)
+        orig_filename='%s.mp4' % os.path.join(settings.MEDIA_ROOT,parent_root)
         new_path = os.path.join(settings.MEDIA_ROOT, 'videos', 'behavioral_event')
         mp4_filename = os.path.join(new_path, '%d.mp4' % self.id)
         if not os.path.exists(mp4_filename):
-            cmds = ['ffmpeg', '-i', '%s.mp4' % os.path.join(settings.MEDIA_ROOT,parent_root), '-ss', start_time_string,
-                    '-t', duration_string, '-vcodec', 'libx264', mp4_filename]
-            print(cmds)
-            subprocess.call(cmds)
+            convert_to_mp4(mp4_filename, orig_filename, start_time=start_time_string, duration=duration_string)
         ogg_filename = os.path.join(new_path, '%d.ogg' % self.id)
         if not os.path.exists(ogg_filename):
-            cmds = ['ffmpeg2theora', mp4_filename, '-o', ogg_filename]
-            subprocess.call(cmds)
+            convert_to_ogg(ogg_filename, mp4_filename, start_time=start_time_string, end_time=end_time_string)
         swf_filename = os.path.join(new_path, '%d.swf' % self.id)
         if not os.path.exists(swf_filename):
-            cmds = ['ffmpeg', '-i', mp4_filename, '-ss', start_time_string, '-t', duration_string, swf_filename]
-            subprocess.call(cmds)
+            convert_to_swf(swf_filename, orig_filename, start_time=start_time_string, duration=duration_string)
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         rename_files = getattr(self, 'RENAME_FILES', None)
@@ -293,17 +245,13 @@ class BehavioralEvent(MPTTModel):
             root,ext=os.path.splitext(orig_filename)
             mp4_filename='%s.mp4' % root
             if not os.path.exists(mp4_filename):
-                cmds=['avconv', '-i', orig_filename, '-vcodec', 'libx264', mp4_filename]
-                print(cmds)
-                subprocess.call(cmds)
+                convert_to_mp4(mp4_filename, orig_filename)
             ogg_filename='%s.ogg' % root
             if not os.path.exists(ogg_filename):
-                cmds=['ffmpeg2theora', orig_filename, '-o', ogg_filename]
-                subprocess.call(cmds)
+                convert_to_ogg(ogg_filename, orig_filename)
             swf_filename='%s.swf' % root
             if not os.path.exists(swf_filename):
-                cmds=['ffmpeg', '-i', orig_filename, swf_filename]
-                subprocess.call(cmds)
+                convert_to_swf(swf_filename, orig_filename)
         else:
             if self.parent is None or self.parent.video.name is None:
                 self.segment_video(self.observation_session.video.name)
