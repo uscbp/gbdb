bff24c16b92a6174279bb8d14f829e8085202a04
jbonaiuto <jbonaiuto@gmail.com>
2014-09-18 18:11:41 +0100

    alternate row colors. fixes #20

--- gbdb/static/gbdb/scripts/commonFunctions.js
+++ gbdb/static/gbdb/scripts/commonFunctions.js
@@ -25,3 +25,11 @@ function updateCircle()
     });
     circle.bindTo('center', marker, 'position');
 }
+
+function getTRTag(idx)
+{
+    if(idx%2==0)
+        return 'even_row';
+    else
+        return 'odd_row';
+}
